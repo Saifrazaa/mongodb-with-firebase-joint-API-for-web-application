e2e2ae9de85aa6f1618a549dcedd296e596861c2
root <razasaif171@gmail.com>
2018-02-09 16:17:58 +0500

    loop error

--- blooddonor/src/Services/mongodb.js
+++ blooddonor/src/Services/mongodb.js
@@ -1,39 +1,61 @@
-import React ,{Component} from "react";
-class Getdonors extends Component{
-  constructor(){
+import React, { Component } from "react";
+interface User{
+  username: string,
+  email: string,
+  address: string,
+  bloodg: string,
+  phoneno:string
+}
+
+class Getdonors extends Component {
+  users:User[] = []
+  constructor() {
     super();
-    this.state={
-      username:[],
-      email   :[],
-      address :[],
-      bloodg  :[],
-      phoneno :[]
+    this.state = {
+      username: [],
+      email: [],
+      address: [],
+      bloodg: [],
+      phoneno: []
     }
   }
-  componentDidMount(){
-      fetch("/api/donors").then(function(response){
-        return response.json();
-      }).then(result=>{
-        for(var i=0;i<result.length ;i++){
-          this.setState({
-            username:result[i].username,
-            email   :result[i].email,
-            address :result[i].address,
-            bloodg  :result[i].bloodgroup,
-            phoneno :result[i].phoneno
-    
-            })
-          }
-        }
-      )
-      console.log(this.state);
-      
+  componentWillMount(){
+    for(var i=0;i<this.users.length ;i++){
+      console.log(this.users[i]);
+    }
   }
-  render(){
+  componentDidMount() {
+    fetch("/api/donors").then(function (response) {
+      return response.json();
+    }).then(result => {
+      // for (var i = 0; i <= result.length; i++) {
+
+      //   this.setState({
+      //     username: result[i].username,
+      //     email: result[i].email,
+      //     address: result[i].address,
+      //     bloodg: result[i].bloodgroup,
+      //     phoneno: result[i].phoneno
+
+      //   })
+      // }
+      result.forEach(user => {
+        // console.log('asdasd',user['username'])
+        
+        this.users.push(user)
+      
+      });
+      
+    }
+    )
+
+
+  }
+  render() {
     return (
-        <div>
-          <li>{this.state.username}</li>
-        </div>
+      <div>
+        <li></li>
+      </div>
     )
   }
 }
